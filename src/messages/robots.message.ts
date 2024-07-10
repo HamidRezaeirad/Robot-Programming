1c7b1aebff562631eb2f0c357a599053fe150dfc
Hamid Rezaeiard <hamid.rezaeirad@gmail.com>
2024-07-10 23:02:53 +0200

    chore [#11] Checking body requests in e2e test

---
+++ src/messages/robots.message.ts
@@ -0,0 +1,5 @@
+export const validation = {
+  dimension: "dimension's input is not correct! Should be like 5 6",
+  position: "position's input is not correct! Should be like 1 2 N (N|E|S|W)",
+  commands: 'commands not found! The accepted inputs: R|L|F',
+};
